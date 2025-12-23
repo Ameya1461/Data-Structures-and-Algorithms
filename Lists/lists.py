4bd278f89df0953c19ae0ea00c558f811cb42fc9
Ameya Hujare <ameya.hujare@gmail.com>
2025-12-23 11:22:53 -0600

    Array Interview questions

--- Lists/lists.py
+++ Lists/lists.py
@@ -59,3 +59,23 @@ while (True):
 					
 average = sum(numlist) / len(numlist) 
 print('Average:', average)
+
+
+# LC 643 --> SLIDING WINDOW APPROACH - FIXED SIZE
+# class Solution:
+#     def findMaxAverage(self, nums: List[int], k: int) -> float:
+#         n = len(nums)
+#         current_sum = 0
+
+#         for i in range(k):
+#             current_sum += nums[i]
+#         max_average = current_sum / k
+
+#         for i in range(k,n):
+#             current_sum += nums[i]
+#             current_sum -= nums[i-k]
+
+#             average = current_sum / k
+#             max_average = max(max_average, average)
+
+#         return max_average
