70b5d862bd6a857d0b2778d4b050afce8d299941
Ameya Hujare <ameya.hujare@gmail.com>
2026-02-02 22:21:13 -0600

    LC

--- Strings/leetcode.py
+++ Strings/leetcode.py
@@ -15,66 +15,66 @@
 
 
 # Q125 - Valid Palindrome
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        charset = []
-        for char in s.lower():
-            if char.isalnum():
-                charset.append(char)
-        # return charset
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         charset = []
+#         for char in s.lower():
+#             if char.isalnum():
+#                 charset.append(char)
+#         # return charset
 
-        left = 0
-        right = len(charset) - 1
-        while left < right:
-            if charset[left] != charset[right]:
-                return False
-            else:
-                left += 1
-                right -= 1
-        return True
+#         left = 0
+#         right = len(charset) - 1
+#         while left < right:
+#             if charset[left] != charset[right]:
+#                 return False
+#             else:
+#                 left += 1
+#                 right -= 1
+#         return True
         
-# Q3
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
-        res = 0
-        l = 0
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res
+# # Q3
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         charSet = set()
+#         res = 0
+#         l = 0
+#         for r in range(len(s)):
+#             while s[r] in charSet:
+#                 charSet.remove(s[l])
+#                 l += 1
+#             charSet.add(s[r])
+#             res = max(res, r - l + 1)
+#         return res
 
-# Q567
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1 = len(s1)
-        n2 = len(s2)
+# # Q567
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         n1 = len(s1)
+#         n2 = len(s2)
 
-        if n1 > n2:
-            return False
+#         if n1 > n2:
+#             return False
         
-        # Initialing storing value of each char as 0
-        count_s1 = [0] * 26
-        count_s2 = [0] * 26
+#         # Initialing storing value of each char as 0
+#         count_s1 = [0] * 26
+#         count_s2 = [0] * 26
 
-        # s1 = [97 98]  s2 = [103 107]  ASCII values
-        for i in range(n1):
-            count_s1[ord(s1[i]) - 97] += 1
-            count_s2[ord(s2[i]) - 97] += 1
+#         # s1 = [97 98]  s2 = [103 107]  ASCII values
+#         for i in range(n1):
+#             count_s1[ord(s1[i]) - 97] += 1
+#             count_s2[ord(s2[i]) - 97] += 1
 
-        if count_s1 == count_s2:
-            return True
+#         if count_s1 == count_s2:
+#             return True
 
-        for i in range(n1,n2):
-            count_s2[ord(s2[i]) - 97] += 1
-            count_s2[ord(s2[i - n1]) - 97] -= 1
+#         for i in range(n1,n2):
+#             count_s2[ord(s2[i]) - 97] += 1
+#             count_s2[ord(s2[i - n1]) - 97] -= 1
 
-            if count_s1 == count_s2:
-                return True
-        return False
+#             if count_s1 == count_s2:
+#                 return True
+#         return False
 
 
 # Q242
